d9a38b5687b96bcc9f654db32600aa093a3acc32
submit507 <submit507@hotmail.com>
2022-12-30 15:55:13 -0500

    polynomial class update

--- Polynomial.java
+++ Polynomial.java
@@ -1,12 +1,56 @@
 import java.util.TreeMap;
 
 public class Polynomial {
-    private TreeMap<Integer, Integer> polynomials;
+    private TreeMap<Integer, Integer> polynomial;
+    private int degree;
 
     public Polynomial() {
-        polynomials = new TreeMap<Integer, Integer>();
+        polynomial = new TreeMap<Integer, Integer>();
     }
 
-    
+    public Polynomial(Polynomial copy) {
+        polynomial = copy.copy();
+    }
+
+    private TreeMap<Integer, Integer> copy() {
+        return polynomial;
+    }
+
+    private void updateDegree(int newDegree) {
+        if (newDegree > degree) {
+            degree = newDegree;
+        }
+    }
+
+    private int compareDegrees(Polynomial poly2) {
+
+        if (degree >= poly2.getDegree()) {
+            return degree;
+        } else {
+            return poly2.getDegree();
+        }
+    }
+
+    private int getDegree() {
+        return degree;
+    }
+
+    public void addTerm(int coefficient, int power) {
+        polynomial.put(power, coefficient);
+        updateDegree(power);
+    }
+
+    public Polynomial add(Polynomial poly2) {
+        Polynomial result = new Polynomial();
+        int newDegree = compareDegrees(poly2);
+
+        for (int i = 0; i <= degree; i++) {
+
+            if (polynomial.get(i) != null) {
+
+            }
+        }
+
+    }
 
 }
